98e648100fb0d4030e5c1ef7e526238a0e748324
chuan137 <chuan137@gmail.com>
2023-07-19 09:37:38 +0200

    Rename package

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
-# Reloader
+# PyReloader
 
 Start given python script and reloads on signal SIGHUP.
 
 Installation:
 
-pip install reloader
+pip install pyreloader
 
 Usage:
 
-Start program: `reloader ./server.py`
+Start program: `pyreloader ./server.py`
 
 Reload program: `kill -HUP <pid of parent process>`
 
--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name = "reloader",
-    version = "0.1",
+    name = "pyreloader",
+    version = "0.1.2",
     author = "Chuan Miao",
     author_email = "chuan137@gmail.com",
-    description = ("Reload a program on signal HUP."),
+    description = ("Reload a python program on kill signal."),
     license = "MIT",
     keywords = "reload server",
-    url = "https://github.com/chuan137/reloader",
-    py_modules=['reloader'],
+    url = "https://github.com/chuan137/pyreloader",
+    py_modules=['pyreloader'],
     long_description_content_type="text/markdown",
     long_description=read('README.md'),
     classifiers=[
@@ -23,6 +23,6 @@ setup(
     ],
     install_requires=[],
     entry_points={'console_scripts': [
-        'reloader = reloader:main',
+        'pyreloader = pyreloader:main',
     ]},
 )
